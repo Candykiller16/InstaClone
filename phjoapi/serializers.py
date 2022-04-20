7508be4c9caf247b392c2a19a84d21ea1bbd3c2b
Candykiller16 <Candykiller16@mail.ru>
2022-04-20 22:44:48 +0300

    added comments to api

--- phjoapi/serializers.py
+++ phjoapi/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from photojournal.models import Post, Profile, Comments, Category
 
 
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comments
+        fields = '__all__'
+
+
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
@@ -17,7 +24,13 @@ class CategoriesSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     owner = ProfileSerializer(many=False)
     category = CategoriesSerializer(many=True)
+    comments = serializers.SerializerMethodField()
+
+    def get_comments(self, obj):
+        comments = obj.comments_set.all()
+        serializer = CommentSerializer(comments, many=True)
+        return serializer.data
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'description', 'slug', 'owner', 'category', 'likes']
+        fields = ['id', 'title', 'description', 'slug', 'owner', 'category', 'likes', 'comments']
