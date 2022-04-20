35415ee78febcfd09aba5fd59de417fdbba08a46
Candykiller16 <Candykiller16@mail.ru>
2022-04-20 22:39:31 +0300

    add fields owner and categories

--- phjoapi/serializers.py
+++ phjoapi/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
-from photojournal.models import Post
+from photojournal.models import Post, Profile, Comments, Category
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['id', 'name', 'email', 'username', 'short_intro', 'bio']
+
+
+class CategoriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name']
 
 
 class PostSerializer(serializers.ModelSerializer):
+    owner = ProfileSerializer(many=False)
+    category = CategoriesSerializer(many=True)
 
     class Meta:
         model = Post
-        fields = '__all__'
-
+        fields = ['id', 'title', 'description', 'slug', 'owner', 'category', 'likes']
