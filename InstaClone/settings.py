e464b572fd7d61df9219671d3b8e0b91a1a85e45
Candykiller16 <Candykiller16@mail.ru>
2022-04-24 23:24:04 +0300

    env v1.0

--- InstaClone/settings.py
+++ InstaClone/settings.py
@@ -14,11 +14,13 @@ import environ
 from datetime import timedelta
 from pathlib import Path
 
-env = environ.Env()
-environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+env = environ.Env()
+environ.Env.read_env('../.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
