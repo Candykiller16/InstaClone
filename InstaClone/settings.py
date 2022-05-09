c8a50fbfbb4870a46aa473d68022649c859a7824
Candykiller16 <Candykiller16@mail.ru>
2022-05-09 21:39:28 +0300

    updated add_users command

--- InstaClone/settings.py
+++ InstaClone/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'modeltranslation',
 
     'photojournal.apps.PhotojournalConfig',
+
+    'django_seed',
 ]
 
 MIDDLEWARE = [
--- photojournal/management/commands/add_users.py
+++ photojournal/management/commands/add_users.py
@@ -1,6 +1,8 @@
+from random import random
+
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
-from django.utils.crypto import get_random_string
+from django_seed import Seed
 
 
 class Command(BaseCommand):
@@ -11,5 +13,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         total = kwargs['total']
+        seeder = Seed.seeder()
         for i in range(total):
-            User.objects.create_user(username=get_random_string(10), email='', password='123' )
+            User.objects.create_user(username=seeder.faker.user_name(), email=seeder.faker.email(), password='123')
+        return 'Done'
+        # seeder = Seed.seeder()
+        # seeder.add_entity(User, total, {
+        #     'username': lambda x: seeder.faker.name(),
+        #     'email': lambda x: seeder.faker.email(),
+        #     'password': 'password123'
+        # })
+        # seeder.execute()
